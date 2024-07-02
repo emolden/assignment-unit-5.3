d158dbd0a10a940a5eb267712d1322f22b00a5ec
Emma Molden <emma.molden007@gmail.com>
2024-07-02 14:46:30 -0500

    created addtoCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,25 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+//Create an empty array named myCollection.
+
+let myCollection = [];
+
+//Create a function named addToCollection
+  //Take in a collection parameter. (This allows the function to be reused to add an album to any array of album objects.)
+  //Take in the album's title, artist, yearPublished as parameters.
+  //Create a new object having the above properties.
+  //NOTE: Your object's properties must have title, artist, and yearPublished in order for this assignment's automated tests to work correctly!
+  //Add the new object to the end of the collection array.
+  //return the newly created object.
+function addtoCollection(collection, title, artist, yearPublished) {
+  const obj = {};
+  obj.title = title;
+  obj.artist = artist;
+  obj.yearPubished = yearPublished;
+  collection.push(obj);
+  return obj;
+}
 
 
 
